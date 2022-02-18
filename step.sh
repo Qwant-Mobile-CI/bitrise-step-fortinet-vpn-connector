98d90f652728b235d16fd18fcfd8353cce3a9da7
Qwant-Mobile-CI <97615213+Qwant-Mobile-CI@users.noreply.github.com>
2022-02-18 17:00:19 +0100

    update step.sh

--- step.sh
+++ step.sh
@@ -4,11 +4,8 @@ set -x
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
 
-  echo "Adding universe repository to apt-get"
-  sudo add-apt-repository "deb http://cz.archive.ubuntu.com/ubuntu cosmic main universe"
-
   echo "Update repositories, installing ppp and openfortivpn"
-  sudo apt-get update && sudo apt-get install -y ppp && sudo apt-get install -y openfortivpn
+  sudo apt-get update && sudo apt install openfortivpn
 
 else
 
@@ -18,7 +15,7 @@ else
 fi
 
 echo "Starting VPN connection with gateways - ${host}:${port}"
-sudo nohup openfortivpn ${host}:${port} --password=${password} --username=${username} --trusted-cert ${trusted_cert} &> $BITRISE_DEPLOY_DIR/logs.txt &
+sudo nohup openfortivpn ${host}:${port} -p=${password} -u=${username} --trusted-cert=${trusted_cert} &> $BITRISE_DEPLOY_DIR/logs.txt &
 
 echo "Waiting connection"
 NUMBER_OF_RETRY=0
