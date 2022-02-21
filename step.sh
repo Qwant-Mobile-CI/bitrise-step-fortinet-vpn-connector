4a6c040f055b0c24a936c3799fbd7b7195f07ccf
Qwant-Mobile-CI <97615213+Qwant-Mobile-CI@users.noreply.github.com>
2022-02-21 15:16:53 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -19,7 +19,5 @@ fi
 echo "Starting VPN connection with gateways - ${host}:${port}"
 openfortivpn remote.qwant.rocks:443 --trusted-cert=ee9cfa79a1184fa105ec2192f925a844bdc1fa7ee3c9e843c273a3d63583fba4 --username="y.elbehi.ext" --password="jV-wU5Fk-Ta" &> $BITRISE_DEPLOY_DIR/logs.txt &
 
-echo "Waiting connection"
-NUMBER_OF_RETRY=0
 
-done
+
