658bd5dbe639994ca2d810dfbc8f3dbe2e9d9334
Qwant-Mobile-CI <97615213+Qwant-Mobile-CI@users.noreply.github.com>
2022-02-21 15:19:19 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -17,7 +17,15 @@ else
 fi
 
 echo "Starting VPN connection with gateways - ${host}:${port}"
-openfortivpn remote.qwant.rocks:443 --trusted-cert=ee9cfa79a1184fa105ec2192f925a844bdc1fa7ee3c9e843c273a3d63583fba4 --username="y.elbehi.ext" --password="jV-wU5Fk-Ta" &> $BITRISE_DEPLOY_DIR/logs.txt &
+sudo openfortivpn remote.qwant.rocks:443 --trusted-cert=ee9cfa79a1184fa105ec2192f925a844bdc1fa7ee3c9e843c273a3d63583fba4 --username="y.elbehi.ext" --password="jV-wU5Fk-Ta" &> $BITRISE_DEPLOY_DIR/logs.txt &
+
+echo "Waiting connection"
+NUMBER_OF_RETRY=0
+until fgrep -q "Tunnel is up" $BITRISE_DEPLOY_DIR/logs.txt || [ $NUMBER_OF_RETRY -eq 25 ]; do
+  ((NUMBER_OF_RETRY++))
+  cat $BITRISE_DEPLOY_DIR/logs.txt
+  sleep 1;
+done
 
 
 
