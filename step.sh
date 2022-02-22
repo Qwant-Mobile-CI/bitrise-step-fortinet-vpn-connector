b5c4f25a48bcfe090e68574d6502150eb976ea14
Qwant-Mobile-CI <97615213+Qwant-Mobile-CI@users.noreply.github.com>
2022-02-22 17:00:45 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -15,9 +15,16 @@ else
   brew install openfortivpn
 
 fi
-
 echo "Starting VPN connection with gateways - ${host}:${port}"
+
+if [[ "$unamestr" == 'Linux' ]]; then
 sudo openfortivpn remote.qwant.rocks:443 --trusted-cert=ee9cfa79a1184fa105ec2192f925a844bdc1fa7ee3c9e843c273a3d63583fba4 --username="y.elbehi.ext" --password="jV-wU5Fk-Ta" &> $BITRISE_DEPLOY_DIR/logs.txt &
+
+else
+
+sudo openfortivpn remote.qwant.rocks:443 --trusted-cert=ee9cfa79a1184fa105ec2192f925a844bdc1fa7ee3c9e843c273a3d63583fba4 --username="y.elbehi.ext" --password="jV-wU5Fk-Ta" --set-dns=0 --pppd-use-peerdns=1 &> $BITRISE_DEPLOY_DIR/logs.txt &
+
+fi
 
 echo "Waiting connection"
 NUMBER_OF_RETRY=0
